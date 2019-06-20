70ac2b2b9d9176ae8c8764217ce03fe1ab26aa82
Andrey Hernandez <andrey_hz@outlook.com>
2019-06-20 14:19:37 -0500

    Dia 4: Crear un objeto rectangulo

---
+++ Day_4:_Create_a_Rectangle_Object.js
@@ -0,0 +1,37 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the Rectangle function
+ */
+function Rectangle(a, b) {
+    var rect = {
+        length: a,
+        width: b,
+        perimeter: a + a + b + b,
+        area: a * b
+    };
+    return rect;
+}
+
